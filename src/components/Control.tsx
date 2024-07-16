25bbd8ce948e541fb2fab609733a24ed9a105072
Michael Briggs <mogglett@hotmail.com>
2024-07-16 21:28:18 +0100

    Control Panel

--- src/components/Control.tsx
+++ src/components/Control.tsx
@@ -38,11 +38,12 @@ const Control = ({
   return (
     <div className={styles.wrapper}>
       <div className={styles.before}>{renderButtons(beforeActions)}</div>
-      <div className={styles.after}>{renderButtons(afterActions)}</div>
       <div className={styles.center}>
         <h1>{title}</h1>
         <p>{description}</p>
       </div>
+
+      <div className={styles.after}>{renderButtons(afterActions)}</div>
     </div>
   );
 };
